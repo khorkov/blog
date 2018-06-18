f4030cababf74d71cf2ced22f6a7e419d42fa022
playrix <web.playrix@yandex.ru>
2018-06-18 13:20:53 +0300

    Fix: Added localization russian and english lang

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   friendly_id :title, use: :slugged
   has_attached_file :image, styles: { large: '740x280>', medium: '300x300>', thumb: '183x183#' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :title, presence: true, length: { in: 3..20  }, uniqueness: true
 end
